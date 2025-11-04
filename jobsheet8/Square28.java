3bd91bd6b3d4d5c67b33e0eda6d51751c7b69ad1
vicodwijaya-ai <vicodwijaya@gmail.com>
2025-11-04 11:29:27 +0700

    Percobaan 2

---
+++ jobsheet8/Square28.java
@@ -0,0 +1,21 @@
+package jobsheet8;
+
+
+import java.util.Scanner;
+
+public class Square28 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int n = sc.nextInt();
+        for (int iOuter = 1; iOuter <= n; iOuter++) {
+            for (int i = 1; i <= n; i++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
