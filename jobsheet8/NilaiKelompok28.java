2748190374515b03472990323077d213c0524bbc
vicodwijaya-ai <vicodwijaya@gmail.com>
2025-11-04 22:48:31 +0700

    Modifikasi Percobaan 4

--- jobsheet8/NilaiKelompok28.java
+++ jobsheet8/NilaiKelompok28.java
@@ -1,29 +1,44 @@
-package jobsheet8;
 
-import  java.util.Scanner;
+import java.util.Scanner;
 
 public class NilaiKelompok28 {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        int i,j, nilai;
+
+        Scanner sc = new Scanner(System.in);
+
+        int i, j, nilai;
         float totalNilai, rataNilai;
+        float rataTertinggi = 0;
+        int kelompokTertinggi = 0;
 
-            
-            i = 1;
-            while ( i <= 6) {
-                System.out.println("Kelompok " + i);
+        i = 1;
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            totalNilai = 0;
 
-                totalNilai = 0;
-                for ( j = 1; j <= 5; j++) {
-                    System.out.print("Nilai dari Kelompok Penilai " + j + ": ");
-                    nilai = sc.nextInt();
-                    totalNilai += nilai;
-                    
-                }
-                rataNilai = totalNilai / 5;
-                System.out.println("Kelompok " + i + " nilai rata- rata = " + rataNilai);
-                i++;
+            for (j = 1; j <= 5; j++) {
+                System.out.print("Nilai dari Kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
             }
-        }
-    }
 
+            rataNilai = totalNilai / 5;
+            System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
+
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+
+            i++;
+        }
+
+        System.out.println("\n-----------------------------------------");
+        System.out.println("Rata-rata nilai tertinggi adalah: " + rataTertinggi);
+        System.out.println("Diraih oleh: Kelompok " + kelompokTertinggi);
+        System.out.println("-----------------------------------------");
+
+        sc.close();
+    }
+}
