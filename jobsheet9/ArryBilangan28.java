2f9a15e5aba90f7aafffd4f723feeb942e898840
vicodwijaya-ai <vicodwijaya@gmail.com>
2025-11-13 00:54:37 +0700

    Modifikasi Percobaan 1

--- jobsheet9/ArryBilangan28.java
+++ jobsheet9/ArryBilangan28.java
@@ -1,19 +1,24 @@
 package jobsheet9;
 
-public class ArryBilangan28 {
-    public static void main(String[] args) {
-        
-        int [] bil = new int [4];
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+import java.util.Scanner;
 
-        
-        System.out.println(bil [0]);
-        System.out.println(bil [1]);
-        System.out.println(bil [2]);
-        System.out.println(bil [3]);
+public class ArryBilangan28 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] bil = {5, 13, -7, 17};
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+
+        }
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+
     }
-    
-} 
+
+}
