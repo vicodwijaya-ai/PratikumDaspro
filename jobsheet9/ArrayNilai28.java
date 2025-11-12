64e7a922d2005a1adb62b07055f4e4619e3e5635
vicodwijaya-ai <vicodwijaya@gmail.com>
2025-11-13 02:44:24 +0700

    Modifikasi Percobaan 2

--- jobsheet9/ArrayNilai28.java
+++ jobsheet9/ArrayNilai28.java
@@ -1,26 +1,27 @@
 package jobsheet9;
 
 import java.util.Scanner;
-public class ArrayNilai28 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
 
-        int[] nilaiAkhir = new int [10];
+public class ArrayNilai28 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int[10];
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
-            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+            System.out.print("Masukan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
-                System.out.println("Mahasiswa ke-" + i + " lulus! ");
-                
-            }else {
-                System.out.println("Mahasiswa ke-" + i + " tidak lulus! ");
-                
+                System.out.println("Mahasiswa ke- " + i + " adalah " + nilaiAkhir[i]);
+            } else if (nilaiAkhir[i] < 70) {
+                System.out.println("Mahasiswa ke- " + i + " adalah " + nilaiAkhir[i]);
             }
+
         }
     }
-    
+
 }
