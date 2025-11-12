92bf00a21f68cf9ae95db2a9c6f797a1d9785a9c
vicodwijaya-ai <vicodwijaya@gmail.com>
2025-11-13 02:08:02 +0700

    Modifikasi Percobaan 2

--- jobsheet9/ArrayNilai28.java
+++ jobsheet9/ArrayNilai28.java
@@ -7,13 +7,19 @@ public class ArrayNilai28 {
 
         int[] nilaiAkhir = new int [10];
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Masukan nilai akhir ke-" + i + " : ");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir [i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus! ");
+                
+            }else {
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus! ");
+                
+            }
         }
     }
     
