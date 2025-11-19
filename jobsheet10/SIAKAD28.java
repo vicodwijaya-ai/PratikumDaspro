716d690cc0df351e7f74e68ca96cc1299683df00
vicodwijaya-ai <vicodwijaya@gmail.com>
2025-11-20 06:04:50 +0700

    Modifikasi Percobaan 4

--- jobsheet10/SIAKAD28.java
+++ jobsheet10/SIAKAD28.java
@@ -7,7 +7,12 @@ public class SIAKAD28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [][] nilai = new int [4][3];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
+
+        int [][] nilai = new int [jumlahMahasiswa][jumlahMatkul];
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
@@ -18,20 +23,20 @@ public class SIAKAD28 {
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai [i][j];
             }
-            System.out.println("Nilai rata rata: " + totalPerSiswa/3);
+            System.out.println("Nilai rata rata: " + totalPerSiswa / jumlahMatkul);
         }
 
         System.out.println("\n===================================");
         System.out.println("Rata- rata Nilai setiap Mata Kuliah:");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < jumlahMatkul; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jumlahMahasiswa; i++) {
                 totalPerMatkul += nilai [i][j];
             }
 
-            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / 4);
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / jumlahMahasiswa);
         }
 
     }
