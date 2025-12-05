a68f976b60f1a011dd601a7ad614c0c081d82dc0
vicodwijaya-ai <vicodwijaya@gmail.com>
2025-12-06 06:50:57 +0700

    Modifikasi Percobaan 4

--- jobsheet11/PengunjungCafe28.java
+++ jobsheet11/PengunjungCafe28.java
@@ -4,8 +4,8 @@ public class PengunjungCafe28 {
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
 
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
 
